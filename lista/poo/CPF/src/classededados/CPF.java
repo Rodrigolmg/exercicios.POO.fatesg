2381ad405eef2bf23c3bf66c4a74b79fb113e13c
Rodrigolmg <rodrigo.leitemg@gmail.com>
2017-09-16 13:04:27 -0300

    Exercicios POO de aprendizado - SENAI Fatesg

--- lista/poo/CPF/src/classededados/CPF.java
+++ lista/poo/CPF/src/classededados/CPF.java
@@ -1,130 +1,52 @@
 package classededados;
 
 public class CPF{
-    //Atributos
-    private String nome;
-    private int numerocpf;
-    private int dia;
-    private int mes;
-    private int ano;
+    //Atributo(s)
+    private long numerocpf;
     
     public int calcularPrimeiroDigito(){
-        /**
-         * p10, p9, p8, p7, p6, p5, p3, p2
-         * Variáveis para armazenamento de valores resultantes
-         * da multiplicação dos números do CPF por seus pesos (de 10 a 2)
-         */
-        int p10, p9, p8, p7, p6, p5, p4, p3, p2;;
-        int soma; // Variável que recebe o somatório de p10, p9, p8, p7, p6, p5, p3, p2
-        int digito; // Variável que recebe o digito verificador
-        
-        p10 = (this.numerocpf / 100000000) * 10;
-        p9 = ((this.numerocpf / 10000000) % 10) * 9;
-        p8 = ((this.numerocpf / 1000000) % 10) * 8;
-        p7 = ((this.numerocpf / 100000) % 10) * 7;
-        p6 = ((this.numerocpf / 10000) % 10) * 6;
-        p5 = ((this.numerocpf / 1000) % 10) * 5;
-        p4 = ((this.numerocpf / 100) % 10) * 4;
-        p3 = ((this.numerocpf / 10) % 10) * 3;
-        p2 = ((this.numerocpf / 1) % 10) * 2;
-        
-        soma = p10 + p9 + p8 + p7 + p6 + p5 + p4 + p3 + p2;
-        
-        digito = 11 - (soma % 11);
-        if (digito > 9){
-            digito = 0;
-            return digito;
-        } else{
-            return digito;
+        long cpf = this.numerocpf / 100;
+        int soma = 0;
+        int digito;
+        for (int i = 2 ; i <= 10; i++) {
+            soma += (cpf % 10) * i;
+            cpf /= 10;
         }
+        
+        digito = 11 - (soma% 11);
+        if (digito > 9) return digito = 0;
+        else return digito;
     }
     
     public int calcularSegundoDigito(int digitop){
-        /**
-         * p11, p10, p9, p8, p7, p6, p5, p3, p2
-         * Variáveis para armazenamento de valores resultantes
-         * da multiplicação dos números do CPF por seus pesos (de 10 a 2)
-         */
-        int p11, p10, p9, p8, p7, p6, p5, p4, p3, p2;
-        int soma; // Variável que recebe o somatório de p11, p10, p9, p8, p7, p6, p5, p3, p2
-        int digito; // Variável que recebe o digito verificador
-        
-        
-        p11 = (this.numerocpf / 100000000) * 11;
-        p10 = ((this.numerocpf / 10000000) % 10) * 10;
-        p9 = ((this.numerocpf / 1000000) % 10) * 9;
-        p8 = ((this.numerocpf / 100000) % 10) * 8;
-        p7 = ((this.numerocpf / 10000) % 10) * 7;
-        p6 = ((this.numerocpf / 1000) % 10) * 6;
-        p5 = ((this.numerocpf / 100) % 10) * 5;
-        p4 = ((this.numerocpf / 10) % 10) * 4;
-        p3 = ((this.numerocpf / 1) % 10) * 3;
-        p2 = digitop * 2;
-        
-        soma = p11 + p10 + p9 + p8 + p7 + p6 + p5 + p4 + p3 + p2;
+        long cpf = this.numerocpf / 100;
+        int soma = 0;
+        int digito;
+        for (int i = 3; i < 11; i++) {
+            soma += (cpf % 10) * i;
+            cpf /= 10;
+        }
+        soma += digitop * 2;
         
         digito = 11 - (soma % 11);
-        
-        if (digito > 9){
-            digito = 0;
-            return digito;
-        } else{
-            return digito;
-        }
+        if (digito > 9) return digito = 0;
+        else return digito;
     }
-
-    public String getNome() {
-        return nome;
+    
+    public boolean validarCPF(int digito1, int digito2){
+        long d2 = this.numerocpf % 10;
+        long d1 = (this.numerocpf / 10) % 10;
+        return digito2 == d2 && digito1 == d1;
     }
+    
+    //Métodos Acessores e Modificadores
 
-    public void setNome(String nome) throws Exception{
-        char[] n = nome.toCharArray();
-        for (int i = 0; i < n.length; i++) {
-            if (Character.isDigit(n[i]))
-                throw new Exception("Nome inválido! Digite novamente");
-            else if (Character.getType(n[i]) == 24)
-                throw new Exception("Nome inválido! Digite novamente");
-            else if (nome.trim().isEmpty())
-                throw new Exception("Nome inválido! Digite novamente");
-        }
-        this.nome = nome;
-    }
-
-    public int getNumeroCpf() {
+    public long getNumerocpf() {
         return numerocpf;
     }
 
-    public void setNumeroCpf(int numerocpf) throws Exception{
-        if (numerocpf < 1000000) throw new Exception ("Número inválido! Digite novamente!");
+    public void setNumerocpf(long numerocpf) throws Exception{
         this.numerocpf = numerocpf;
     }
-
-    public int getDia() {
-        return dia;
-    }
-
-    public void setDia(int dia) throws Exception{
-        if (dia <= 0) throw new Exception ("Dia inválido! Digite novamente!");
-        this.dia = dia;
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) throws Exception{
-        if (mes <=0 || mes > 12) throw new Exception ("Mês inválido! Digite novamente!");
-        this.mes = mes;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) throws Exception{
-        if (ano < 1900 || ano > 2017) throw new Exception ("Ano inválido! Digite novamente!");
-        this.ano = ano;
-    }
-    
     
 }
--- lista/poo/CPF/src/modelografico/ModeloGraficoCPF.java
+++ lista/poo/CPF/src/modelografico/ModeloGraficoCPF.java
@@ -15,40 +15,25 @@ public class ModeloGraficoCPF extends javax.swing.JFrame {
 
         jLabelTitleCPF = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
-        jLabelNome = new javax.swing.JLabel();
-        jLabelDataNasc = new javax.swing.JLabel();
         jLabelCPF = new javax.swing.JLabel();
-        jButtonCalcDigito = new javax.swing.JButton();
+        jButtonVerificar = new javax.swing.JButton();
         jButtonLimpar = new javax.swing.JButton();
-        jTextFieldNome = new javax.swing.JTextField();
         jTextFieldCPF = new javax.swing.JTextField();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextAreaResults = new javax.swing.JTextArea();
-        jTextFieldDia = new javax.swing.JTextField();
-        jLabelBarra1 = new javax.swing.JLabel();
-        jTextFieldMes = new javax.swing.JTextField();
-        jLabelBarra2 = new javax.swing.JLabel();
-        jTextFieldAno = new javax.swing.JTextField();
+        jLabelResultado = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabelTitleCPF.setFont(new java.awt.Font("Franklin Gothic Book", 0, 18)); // NOI18N
         jLabelTitleCPF.setText("CPF");
 
-        jLabelNome.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
-        jLabelNome.setText("Nome completo:");
-
-        jLabelDataNasc.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
-        jLabelDataNasc.setText("Data de nascimento");
-
         jLabelCPF.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
         jLabelCPF.setText("Número de CPF:");
 
-        jButtonCalcDigito.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
-        jButtonCalcDigito.setText("Calcular dígito");
-        jButtonCalcDigito.addActionListener(new java.awt.event.ActionListener() {
+        jButtonVerificar.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
+        jButtonVerificar.setText("Verificar dígito");
+        jButtonVerificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonCalcDigitoActionPerformed(evt);
+                jButtonVerificarActionPerformed(evt);
             }
         });
 
@@ -60,16 +45,9 @@ public class ModeloGraficoCPF extends javax.swing.JFrame {
             }
         });
 
-        jTextAreaResults.setEditable(false);
-        jTextAreaResults.setColumns(20);
-        jTextAreaResults.setRows(5);
-        jScrollPane1.setViewportView(jTextAreaResults);
-
-        jLabelBarra1.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
-        jLabelBarra1.setText("/");
-
-        jLabelBarra2.setFont(new java.awt.Font("Franklin Gothic Book", 0, 14)); // NOI18N
-        jLabelBarra2.setText("/");
+        jLabelResultado.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabelResultado.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelResultado.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,43 +57,21 @@ public class ModeloGraficoCPF extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1))
+                        .addGap(163, 163, 163)
+                        .addComponent(jLabelTitleCPF))
                     .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabelCPF)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextFieldCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(jButtonVerificar)
                         .addGap(47, 47, 47)
-                        .addComponent(jButtonCalcDigito)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButtonLimpar)
-                        .addGap(44, 44, 44))
+                        .addComponent(jButtonLimpar))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabelNome)
-                                .addGap(18, 18, 18)
-                                .addComponent(jTextFieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                    .addComponent(jLabelDataNasc)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addComponent(jTextFieldDia, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jLabelBarra1)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jTextFieldMes, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jLabelBarra2)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jTextFieldAno))
-                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                    .addComponent(jLabelCPF)
-                                    .addGap(18, 18, 18)
-                                    .addComponent(jTextFieldCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addGap(0, 22, Short.MAX_VALUE)))
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(163, 163, 163)
-                .addComponent(jLabelTitleCPF)
+                        .addComponent(jLabelResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -125,29 +81,17 @@ public class ModeloGraficoCPF extends javax.swing.JFrame {
                 .addComponent(jLabelTitleCPF)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelNome)
-                    .addComponent(jTextFieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelDataNasc)
-                    .addComponent(jTextFieldDia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabelBarra1)
-                    .addComponent(jTextFieldMes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabelBarra2)
-                    .addComponent(jTextFieldAno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(30, 30, 30)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabelCPF)
                     .addComponent(jTextFieldCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonCalcDigito)
-                    .addComponent(jButtonLimpar))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 139, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonVerificar)
+                    .addComponent(jButtonLimpar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         pack();
@@ -158,39 +102,30 @@ public class ModeloGraficoCPF extends javax.swing.JFrame {
     CPF cpf = new CPF();
     
     
-    private void jButtonCalcDigitoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCalcDigitoActionPerformed
+    private void jButtonVerificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonVerificarActionPerformed
         int pdigito; // Primeiro Dígito
         int sdigito; // Segundo Dígito
         try{
             //Entrada de dados
-            cpf.setNome(jTextFieldNome.getText());
-            cpf.setNumeroCpf(Integer.parseInt(jTextFieldCPF.getText()));
-            cpf.setDia(Integer.parseInt(jTextFieldDia.getText()));
-            cpf.setMes(Integer.parseInt(jTextFieldMes.getText()));
-            cpf.setAno(Integer.parseInt(jTextFieldAno.getText()));
-            
+            cpf.setNumerocpf(Long.parseLong(jTextFieldCPF.getText()));
             pdigito = cpf.calcularPrimeiroDigito();
             sdigito = cpf.calcularSegundoDigito(pdigito);
             
-            //Saída
-            String saida = "Nome: " + cpf.getNome() + "\n";
-            saida += "Data de nascimento: " + cpf.getDia() + " de " + cpf.getMes() + " de " + cpf.getAno() + "\n";
-            saida += "CPF + Digitos de Verificação: " + cpf.getNumeroCpf() + " - " + pdigito + sdigito + "\n";
+            //Objeto para saída
+            String saida;
             
-            jTextAreaResults.setText(saida);
+            if (cpf.validarCPF(pdigito, sdigito) == true) saida = "CPF válido!";
+            else saida = "CPF inválido!";
+            jLabelResultado.setText(saida);
             
         } catch (NumberFormatException e){
             JOptionPane.showMessageDialog(rootPane, "Espaço vazio, letras ou símbolos não permitidos!", "Erro!", 0);
         } catch (Exception er){
             JOptionPane.showMessageDialog(rootPane, er.getMessage(), "Erro!", 0);
         }
-    }//GEN-LAST:event_jButtonCalcDigitoActionPerformed
+    }//GEN-LAST:event_jButtonVerificarActionPerformed
 
     private void jButtonLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLimparActionPerformed
-        jTextFieldNome.setText("");
-        jTextFieldDia.setText("");
-        jTextFieldMes.setText("");
-        jTextFieldAno.setText("");
         jTextFieldCPF.setText("");
     }//GEN-LAST:event_jButtonLimparActionPerformed
 
@@ -227,21 +162,12 @@ public class ModeloGraficoCPF extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonCalcDigito;
     private javax.swing.JButton jButtonLimpar;
-    private javax.swing.JLabel jLabelBarra1;
-    private javax.swing.JLabel jLabelBarra2;
+    private javax.swing.JButton jButtonVerificar;
     private javax.swing.JLabel jLabelCPF;
-    private javax.swing.JLabel jLabelDataNasc;
-    private javax.swing.JLabel jLabelNome;
+    private javax.swing.JLabel jLabelResultado;
     private javax.swing.JLabel jLabelTitleCPF;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextArea jTextAreaResults;
-    private javax.swing.JTextField jTextFieldAno;
     private javax.swing.JTextField jTextFieldCPF;
-    private javax.swing.JTextField jTextFieldDia;
-    private javax.swing.JTextField jTextFieldMes;
-    private javax.swing.JTextField jTextFieldNome;
     // End of variables declaration//GEN-END:variables
 }
